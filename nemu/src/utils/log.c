57930de608a5b0e1375edda8a8728c9f8a6643c1
tracer-ysyx <tracer@ysyx.org>
2023-11-04 15:34:42 +0800

    > compile NEMU ysyx_22040000 张三 Linux london 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:34:42 up  5:07,  1 user,  load average: 1.18, 0.97, 0.75

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -184,6 +184,8 @@ void ftrace_check_address(int func_type, uint32_t pc, uint32_t address){
       break;
     }
     function_log_write("%s\n", ftrace_input);
+  }else{
+    assert(0);
   }
   free(ftrace_input);
 }
