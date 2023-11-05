75ad2d1e85949b865bbd0d752285d285326f21b9
tracer-ysyx <tracer@ysyx.org>
2023-11-05 15:58:54 +0800

    > compile NEMU ysyx_22040000 张三 Linux london 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:58:54 up 1 day,  5:31,  1 user,  load average: 0.78, 0.76, 0.54

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -14,8 +14,7 @@
 ***************************************************************************************/
 
 #include <isa.h>
-
-
+#include <cpu/difftest.h>
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
@@ -24,8 +23,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   cpu.csr.mcause = NO;
   cpu.csr.mepc = epc;
 
-  extern bool is_skip_ref;
-  is_skip_ref = true;
+  difftest_skip_ref();
 
 #ifdef CONFIG_XTRACE
   exception_log_write("pc is 0x%x, raise intr with exception number is %d\n", epc, NO);
