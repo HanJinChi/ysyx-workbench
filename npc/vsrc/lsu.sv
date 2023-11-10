1f03e6593e9b1b7ef13258fd3e802dc379e45df8
tracer-ysyx <tracer@ysyx.org>
2023-11-10 21:47:49 +0800

    > sim RTL ysyx_22040000 张三 Linux london 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:47:49 up 6 days, 11:20,  1 user,  load average: 2.89, 2.07, 1.53

--- npc/vsrc/lsu.sv
+++ npc/vsrc/lsu.sv
@@ -14,17 +14,17 @@ module lsu (
   
   reg  [31:0]     data;
   wire            sram_valid;
-  // sram srb(
-  //   // .clk(clk),
-  //   // .rst(rst),
-  //   .ren(ren),
-  //   .wen(wen),
-  //   .wmask(wmask),
-  //   .addr(exu_result),
-  //   .wdata(rsb),
-  //   .data(data)
-  //   // .sram_valid(sram_valid)
-  // );
+  sram srb(
+    // .clk(clk),
+    // .rst(rst),
+    .ren(ren),
+    .wen(wen),
+    .wmask(wmask),
+    .addr(exu_result),
+    .wdata(rsb),
+    .data(data)
+    // .sram_valid(sram_valid)
+  );
 
   MuxKeyWithDefault #(3, 32, 32) rwd(memory_read_wd, rmask, 32'h0, {
     32'h000000ff, memory_read_signed ? {{24{data[7]}} , data[7:0]}  : data & rmask,
@@ -32,12 +32,6 @@ module lsu (
     32'hffffffff, data
   });
 
-  always @(*) begin
-    if(ren)    n_pmem_read(exu_result, data);
-    else       data = 0;
-    if(wen)    n_pmem_write(exu_result, rsb, wmask);
-    else       n_pmem_write(exu_result, rsb, 0);
-  end
   
   // assign wbu_valid = (ren == 1) ? (sram_valid) : 1; // 只有取值命令才需要等待sram返回值
     
