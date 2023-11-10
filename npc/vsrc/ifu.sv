3f3284070acfc3ea04cd77ebd416cdd84217e9de
tracer-ysyx <tracer@ysyx.org>
2023-11-10 10:17:10 +0800

    > sim RTL ysyx_22040000 张三 Linux london 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:17:10 up 5 days, 23:49,  1 user,  load average: 0.73, 0.58, 0.54

--- npc/vsrc/ifu.sv
+++ npc/vsrc/ifu.sv
@@ -6,34 +6,39 @@ module ifu(
     output     [31:0]     instruction
 );
 
-  // wire   [31:0]      data;
-  // wire               ren;
-  // wire               wen;
-  // wire   [7: 0]      wmask;
-  // wire   [31:0]      wdata;
-  // wire               sram_valid;   
- 
-  // sram sr(
-  //   // .clk(clk),
-  //   // .rst(rst),
-  //   .ren(ren),
-  //   .wen(wen),
-  //   .wmask(wmask),
-  //   .addr(pc_next),
-  //   .wdata(wdata),
-  //   .data(instruction)
-  //   // .sram_valid(sram_valid)
-  // );
+  wire   [31:0]      data;
+  wire               ren;
+  wire               wen;
+  wire   [7: 0]      wmask;
+  wire   [31:0]      wdata;
+  wire               sram_valid;   
+  reg    [31:0]      reg_pc_next;
 
-  // assign wen = 1'b0; // 写不使能
-  // assign wmask = 8'b0;
-  // assign wdata = 32'h0;
+
+  Reg #(32, 32'h80000000) regdx(clk, rst, pc_next, reg_pc_next, 1); // assign pc value
+
+ 
+  sram sr(
+    // .clk(clk),
+    // .rst(rst),
+    .ren(ren),
+    .wen(wen),
+    .wmask(wmask),
+    .addr(reg_pc_next),
+    .wdata(wdata),
+    .data(instruction)
+    // .sram_valid(sram_valid)
+  );
+
+  assign wen = 1'b0; // 写不使能
+  assign wmask = 8'b0;
+  assign wdata = 32'h0;
   // assign idu_valid = sram_valid;
 
-  always@(posedge clk) begin
-    if(!rst) begin
-      n_pmem_read(pc_next, instruction);
-    end
-  end 
+  // always@(posedge clk) begin
+  //   if(!rst) begin
+  //     n_pmem_read(pc_next, instruction);
+  //   end
+  // end 
 
 endmodule
