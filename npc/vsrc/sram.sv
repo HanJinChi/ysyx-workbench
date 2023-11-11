570bbe2032f48bc1d2f3da593907d7328e4a4797
tracer-ysyx <tracer@ysyx.org>
2023-11-11 15:51:14 +0800

    > sim RTL ysyx_22040000 张三 Linux london 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:51:14 up 7 days,  5:23,  1 user,  load average: 0.77, 0.44, 0.35

--- npc/vsrc/sram.sv
+++ npc/vsrc/sram.sv
@@ -10,7 +10,7 @@ module sram(
     output              sram_valid
 );
   parameter S0 = 0, S1 = 1;
-  reg reg_valid;
+  reg reg_read_valid; // reg_read_valid代表读是否可用，与写没有关系
   reg sram_state, sram_next_state;
   reg [31:0] reg_data;
   wire read_memory;
@@ -19,7 +19,7 @@ module sram(
       sram_state <= sram_next_state;
     end else begin
       sram_state <= S0;
-      reg_valid <= 0;
+      reg_read_valid <= 0;
     end
   end
 
@@ -37,25 +37,23 @@ module sram(
     endcase
   end
 
-  // always@(posedge clk) begin
-  //   if(!rst) begin
-  //     if(sram_next_state == S1) 
-  //       reg_valid <= 1;
-  //     else begin
-  //       if(wen == 1) reg_valid <= 1;
-  //       if(ren == 1) reg_valid <= 0;
-  //     end
-  //   end
-  // end
+  always@(posedge clk) begin
+    if(!rst) begin
+      if(sram_next_state == S1) 
+        reg_read_valid <= 1;
+      else begin
+        if(ren == 1) reg_read_valid <= 0;
+      end
+    end
+  end
 
   always @(*) begin
-    if(sram_valid) n_pmem_read(addr, reg_data);    
+    if(reg_read_valid) n_pmem_read(addr, reg_data);    
     else    reg_data = 32'h0;
     if(wen) n_pmem_write(addr, wdata, wmask);
     else    n_pmem_write(addr, wdata, 0);
   end
-  // assign sram_valid = reg_valid;
-  assign sram_valid = wen | (sram_state == S1) | !ren;
+  assign sram_valid = wen | reg_read_valid;
   assign data = reg_data;
 
 endmodule
