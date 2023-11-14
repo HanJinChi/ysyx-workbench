2bbe5567742f4700d9bff65d54ec994043cc1d01
tracer-ysyx <tracer@ysyx.org>
2023-11-14 11:11:52 +0800

    > compile NEMU ysyx_22040000 张三 Linux london 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:11:52 up 10 days, 44 min,  1 user,  load average: 0.41, 0.45, 0.60

--- npc/vsrc/ifu.sv
+++ npc/vsrc/ifu.sv
@@ -18,41 +18,50 @@ module ifu(
   reg                reg_ren;
 
  
-  sram sr(
-    .clk(clk),
-    .rst(rst),
-    .ren(ren),
-    .wen(wen),
-    .wmask(wmask),
-    .sram_receive_valid(ifu_receive_valid),
-    .addr(sram_addr),
-    .wdata(wdata),
-    .data(instruction),
-    .sram_valid(sram_valid)
-  );
+  // sram sr(
+  //   .clk(clk),
+  //   .rst(rst),
+  //   .ren(ren),
+  //   .wen(wen),
+  //   .wmask(wmask),
+  //   .sram_receive_valid(ifu_receive_valid),
+  //   .addr(sram_addr),
+  //   .wdata(wdata),
+  //   .data(instruction),
+  //   .sram_valid(sram_valid)
+  // );
 
-  assign wen = 1'b0; // 写不使能
-  assign wmask = 8'b0;
-  assign wdata = 32'h0;
-  assign ren = reg_ren;
-  assign ifu_send_valid = sram_valid;
-  assign sram_addr = (reg_pc_next == 32'h0) ? 32'h80000000 : reg_pc_next;
+  // assign wen = 1'b0; // 写不使能
+  // assign wmask = 8'b0;
+  // assign wdata = 32'h0;
+  // assign ren = reg_ren;
+  // assign ifu_send_valid = sram_valid;
+  // assign sram_addr = (reg_pc_next == 32'h0) ? 32'h80000000 : reg_pc_next;
 
-  always@(posedge clk) begin
-    if(rst) begin
-      reg_pc_next <= 32'h80000000;
-      reg_ren <= 0;
-    end else begin
-      if(ifu_receive_valid) begin
-        reg_pc_next <= pc_next;
-        reg_ren <= 1;
-      end else begin
-        reg_ren <= 0;
-      end
-    end
+  // always@(posedge clk) begin
+  //   if(rst) begin
+  //     reg_pc_next <= 32'h80000000;
+  //     reg_ren <= 0;
+  //   end else begin
+  //     if(ifu_receive_valid) begin
+  //       reg_pc_next <= pc_next;
+  //       reg_ren <= 1;
+  //     end else begin
+  //       reg_ren <= 0;
+  //     end
+  //   end
+  // end
 
-
-  end
+  axi_sram(
+    .aclk(clk),
+    .areset(rst),
+    .araddr(pc_next),
+    .arvalid(),
+    .rready(),
+    .arready(),
+    .data(),
+    .rvalid(),
+  )
 
 
 
