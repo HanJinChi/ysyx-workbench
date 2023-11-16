cbd88c2c383ee2606608a3381ed8bdd230a509d5
hanjinchi <hanjinchi00@outlook.com>
2023-11-16 10:01:18 +0800

    add axi-lite's part of read

---
+++ npc/vsrc/axi_sram.sv
@@ -0,0 +1,165 @@
+module axi_sram  #(SRAM_READ_CYCLE = 1)(
+    input    wire          aclk,
+    input    wire          areset,
+    input    wire  [31:0]  araddr,  // 要读的地址
+    input    wire          arvalid, 
+    input    wire          rready,
+    input    wire  [31:0]  awaddr,  // 要写的地址
+    input    wire          awvalid,
+    input    wire  [31:0]  wdata,   // 要写的数据
+    input    wire  [7 :0]  wstrb,   // 写掩码 
+    input    wire          wvalid,   
+    output   wire          arready,
+    output   reg   [31:0]  data,
+    output   reg           rvalid,
+    output   wire          awready,
+    output   wire          wready
+);
+
+  reg  reg_arready;
+  assign arready = reg_arready;
+
+  always @(posedge aclk) begin
+    if(areset) begin  //  高电平复位有效
+      reg_arready <= 0;  // 默认将arready设为1
+    end
+    else begin
+      reg_arready <= 1;
+    end
+  end
+
+  reg [31:0] reg_araddr;
+  always @(posedge aclk) begin
+    if(areset) begin
+      reg_araddr <= 0;
+    end else begin
+      if(arvalid && arready) begin
+        reg_araddr <= araddr;
+      end
+    end 
+  end
+
+  parameter S0 = 0, S1 = 1;
+  reg sram_read_state, sram_read_next_state;
+  always @(posedge aclk) begin
+    if(areset) begin
+      sram_read_state <= S0;
+    end else begin
+      sram_read_state <= sram_read_next_state;
+    end
+  end
+
+  always@(sram_read_state or arvalid or arready) begin
+    case(sram_read_state)
+    S0: begin
+      if(arvalid && arready) begin
+        sram_read_next_state = S1;
+      end else begin
+        sram_read_next_state = S0;
+      end
+    end
+    S1: begin
+      sram_read_next_state = S0;
+    end
+    default: begin end // do nothing
+    endcase
+  end
+
+  reg [31:0] reg_read_data;
+  always@(*) begin
+    if(sram_read_state == S1) n_pmem_read(reg_araddr, reg_read_data);
+    else                      reg_read_data = 0;
+  end
+
+  reg wait_for_read;
+  always @(posedge aclk) begin
+    if(areset) begin
+      rvalid <= 0;
+      data  <= 0;
+      wait_for_read <= 0;
+    end else begin
+      if(wait_for_read) begin
+        if(rready) begin
+          assert(rvalid == 1);
+          wait_for_read <= 0;
+          rvalid <= 0;
+        end
+      end else begin
+        if(sram_read_state == S1) begin
+          assert(rvalid == 0);
+          rvalid <= 1;
+          data <= reg_read_data;
+          if(!rready) begin
+            wait_for_read <= 1;
+          end
+        end else begin
+          if(rvalid && rready) begin
+            rvalid <= 0;
+          end
+        end
+      end
+    end
+  end
+
+  reg  reg_awready;
+  assign awready = reg_awready;
+
+  always @(posedge aclk) begin
+    if(areset) begin  
+      reg_awready <= 0;  
+    end
+    else begin
+      reg_awready <= 1;
+    end
+  end
+
+  reg [31:0] reg_awaddr;
+  always @(posedge aclk) begin
+    if(areset) begin
+      reg_awaddr <= 0;
+    end else begin
+      if(awvalid && awready) begin
+        reg_awaddr <= awaddr;
+      end
+    end 
+  end
+
+  reg  reg_wready;
+  assign wready = reg_wready;
+
+  always @(posedge aclk) begin
+    if(areset) begin  
+      reg_wready <= 0;  
+    end
+    else begin
+      reg_wready <= 1;
+    end
+  end
+
+  reg [31:0] reg_wdata;
+  reg [7 :0] reg_wstrb;
+  reg        ready_to_write;
+  always @(posedge aclk) begin
+    if(areset) begin
+      reg_wdata <= 0;
+      ready_to_write <= 0;
+    end else begin
+      if(wvalid && wready) begin
+        reg_wdata <= wdata;
+        reg_wstrb <= wstrb;
+        ready_to_write <= 1;
+      end 
+    end
+  end
+
+  wire [31:0]  write_addr; // 真实要写入的地址,这时候分两种情况: 1.地址在数据传输之前已经获得,因此要取reg_addr 2.数据传输和地址传输同时到达(大部分情况),因此直接取awaddr
+  assign write_addr = (awvalid && awready) ? awaddr : reg_awaddr;
+  always @(*) begin
+    if(ready_to_write) begin
+      n_pmem_write(write_addr, reg_wdata, reg_wstrb);
+    end else begin
+      n_pmem_write(write_addr, reg_wdata, 0);
+    end
+  end
+
+endmodule
--- npc/vsrc/ifu.sv
+++ npc/vsrc/ifu.sv
@@ -4,54 +4,87 @@ module ifu(
     input      [31:0]     pc_next,
     input                 ifu_receive_valid,
     output                ifu_send_valid,
-    output  reg   [31:0]  instruction
+    output     [31:0]     instruction
 );
 
-  wire   [31:0]      data;
-  wire               ren;
-  wire               wen;
-  wire   [7: 0]      wmask;
-  wire   [31:0]      wdata;
-  wire               sram_valid;
-  wire   [31:0]      sram_addr;   
-  reg    [31:0]      reg_pc_next;
-  reg                reg_ren;
+  wire awready, wready;
 
- 
-  sram sr(
-    .clk(clk),
-    .rst(rst),
-    .ren(ren),
-    .wen(wen),
-    .wmask(wmask),
-    .sram_receive_valid(ifu_receive_valid),
-    .addr(sram_addr),
-    .wdata(wdata),
-    .data(instruction),
-    .sram_valid(sram_valid)
+  axi_sram axi(
+    .aclk(clk),
+    .areset(rst),
+    .araddr(araddr),
+    .arvalid(arvalid),
+    .rready(rready),
+    .awaddr(0),
+    .awvalid(0),
+    .wdata(0),
+    .wstrb(0),
+    .wvalid(0),
+    .awready(awready),
+    .wready(wready),
+    .arready(arready),
+    .data(data),
+    .rvalid(rvalid)
   );
 
-  assign wen = 1'b0; // 写不使能
-  assign wmask = 8'b0;
-  assign wdata = 32'h0;
-  assign ren = reg_ren;
-  assign ifu_send_valid = sram_valid;
-  assign sram_addr = (reg_pc_next == 32'h0) ? 32'h80000000 : reg_pc_next;
-
+  reg arvalid, rready;
+  reg wait_for_read_address;
+  reg [31:0] araddr;
+  wire arready;
+  wire rvalid;
+  wire [31:0] data;
   always@(posedge clk) begin
-    if(!rst) begin
-      if(ifu_receive_valid) begin
-        reg_pc_next <= pc_next;  
-        reg_ren <= 1;
-      end else
-        reg_ren <= 0; //  只有一个周期发出ren命令即可
-    end
-    else begin
-      reg_pc_next <= 32'h80000000;
-      reg_ren <= 0;
+    if(rst) begin
+      arvalid <= 0;
+      araddr <= 0;
+      wait_for_read_address <=0 ;
+    end else begin
+      if(wait_for_read_address) begin
+        if(arready) begin
+          assert(arvalid == 1);
+          wait_for_read_address <= 0;
+          arvalid <= 0;
+        end
+      end else begin
+        if(ifu_receive_valid) begin
+          assert(arvalid == 0);
+          arvalid <= 1;
+          araddr <= pc_next;
+          if(!arready) begin
+            wait_for_read_address <= 1;
+          end
+        end else begin
+          if(arvalid && arready) begin
+            arvalid <= 0;
+          end
+        end
+      end
     end
   end
-
+  always@(posedge clk) begin
+    if(rst) begin 
+      rready <= 0;
+    end else begin
+      rready <= 1;
+    end
+  end
+  reg [31:0] reg_data;
+  reg r_ifu_send_valid;
+  always @(posedge clk) begin
+    if(rst) begin
+      reg_data <= 0;
+      r_ifu_send_valid <= 0;
+    end else begin
+      if(rvalid && rready) begin
+        reg_data <= data;
+        r_ifu_send_valid <= 1;
+      end else begin
+        r_ifu_send_valid <= 0;
+      end
+    end
+  end
+  assign instruction = reg_data;
+  assign ifu_send_valid = r_ifu_send_valid;
 
 
 endmodule
