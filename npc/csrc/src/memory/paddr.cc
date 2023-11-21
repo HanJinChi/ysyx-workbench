0f9a9cd1a102b455860bcc0a70b5b2fefa24c624
tracer-ysyx <tracer@ysyx.org>
2023-11-21 23:25:08 +0800

    > sim RTL ysyx_22040000 张三 Linux london 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:25:08 up 17 days, 12:57,  1 user,  load average: 0.60, 0.55, 0.65

--- npc/csrc/src/memory/paddr.cc
+++ npc/csrc/src/memory/paddr.cc
@@ -40,6 +40,7 @@ word_t paddr_read(paddr_t addr, int len) {
   #endif
     return data;
   }
+  if(addr == 0) return 0;
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
