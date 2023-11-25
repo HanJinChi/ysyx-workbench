1d046eb2518a835b8c847183f4e1af0168f2caba
tracer-ysyx <tracer@ysyx.org>
2023-11-25 15:21:38 +0800

    > compile NEMU ysyx_22040000 张三 Linux london 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:21:38 up 21 days,  4:54,  1 user,  load average: 0.41, 0.60, 0.49

--- npc/csrc/src/isa/init_isa.cc
+++ npc/csrc/src/isa/init_isa.cc
@@ -1,14 +1,14 @@
 #include <memory/paddr.h>
 
 static const uint32_t img [] = {
-  0x00a00713, // li a4, 10     
-  0x00000297, // auipc t0,0
-  // 0x00028823,  // sb  zero,16(t0)
-  // 0x0102c503,  // lbu a0,16(t0)
-  0x00050793, // mv	a5,a0
-  0x00058693, // mv	a3,a1
-  0x00060713, // mv	a4,a2
+  0x00000297,  // auipc t0,0
+  0x00a00713,  // li  a4,10     
+  // 0x00028823,   // sb  zero,16(t0)
+  // 0x0102c503,   // lbu a0,16(t0)
+  0x00050793,  // mv	a5,a0
+  0x00058693,  // mv	a3,a1
   0x00028823,  // sb  zero,16(t0)
+  0x00060713,  // mv	a2,a4
   0x00100073,  // ebreak (used as nemu_trap)
   0xdeadbeef,  // some data
 };
