32657f58720387f18d15f7bb889ef53e9efdb755
tracer-ysyx <tracer@ysyx.org>
2023-11-27 10:18:57 +0800

    > sim RTL ysyx_22040000 张三 Linux london 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:18:57 up 22 days, 23:51,  1 user,  load average: 0.79, 0.73, 0.77

--- npc/vsrc/axi_sram.sv
+++ npc/vsrc/axi_sram.sv
@@ -43,11 +43,11 @@ module axi_sram  #(SRAM_READ_CYCLE = 1)(
     end 
   end
 
-  parameter IDLE = 0, MEM_READ = 1;
+  parameter IDLE_R = 0, MEM_READ = 1;
   reg sram_read_state, sram_read_next_state;
   always @(posedge aclk) begin
     if(areset) begin
-      sram_read_state <= IDLE;
+      sram_read_state <= IDLE_R;
     end else begin
       sram_read_state <= sram_read_next_state;
     end
@@ -55,15 +55,15 @@ module axi_sram  #(SRAM_READ_CYCLE = 1)(
 
   always@(*) begin
     case(sram_read_state)
-    IDLE: begin
+    IDLE_R: begin
       if(arvalid && arready) 
         sram_read_next_state = MEM_READ;
       else 
-        sram_read_next_state = IDLE;
+        sram_read_next_state = IDLE_R;
     end
     MEM_READ: begin
       if(rvalid && rready) 
-        sram_read_next_state = IDLE;
+        sram_read_next_state = IDLE_R;
       else
         sram_read_next_state = MEM_READ;
     end
@@ -78,40 +78,6 @@ module axi_sram  #(SRAM_READ_CYCLE = 1)(
   end
 
   reg wait_for_read;
-  // always @(posedge aclk) begin
-  //   if(areset) begin
-  //     rvalid <= 0;
-  //     rdata  <= 0;
-  //     rresp  <= 1;  // 1 代表 exokay
-  //     wait_for_read <= 0;
-  //   end else begin
-  //     if(wait_for_read) begin
-  //       if(rready) begin
-  //         assert(rvalid == 1);
-  //         wait_for_read <= 0;
-  //         rvalid <= 0;
-  //         rresp <= 1;
-  //       end
-  //     end else begin
-  //       if(sram_read_state == S1) begin
-  //         assert(rvalid == 0);
-  //         assert(rresp == 1);
-  //         rvalid <= 1;
-  //         rdata <= reg_read_data;
-  //         rresp <= 0;
-  //         if(!rready) begin
-  //           wait_for_read <= 1;
-  //         end
-  //       end else begin
-  //         if(rvalid && rready) begin
-  //           rvalid <= 0;
-  //           rresp  <= 1;
-  //         end
-  //       end
-  //     end
-  //   end
-  // end
-
   always @(posedge aclk) begin
     if(areset) begin
       rvalid        <= 0;
@@ -142,7 +108,6 @@ module axi_sram  #(SRAM_READ_CYCLE = 1)(
         end
       end
     end
-
   end
 
 
@@ -194,10 +159,38 @@ module axi_sram  #(SRAM_READ_CYCLE = 1)(
     end
   end
 
+  parameter IDLE_W = 0, MEM_WRITE = 1;
+  reg sram_write_state, sram_write_next_state;
+  always @(posedge aclk) begin
+    if(areset) begin
+      sram_write_state <= IDLE_W;
+    end else begin
+      sram_write_state <= sram_write_next_state;
+    end
+  end
+
+  always@(*) begin
+    case(sram_write_state)
+    IDLE_W: begin
+      if(wvalid && wready)
+        sram_write_next_state = MEM_WRITE;
+      else 
+        sram_write_next_state = IDLE_W;
+    end
+    MEM_WRITE: begin
+      if(bvalid && bready)
+        sram_write_next_state = IDLE_W;
+      else 
+        sram_read_next_state = MEM_WRITE;
+    end
+    default: begin end // do nothing
+    endcase
+  end
+
   wire [31:0]  write_addr; // 真实要写入的地址,这时候分两种情况: 1.地址在数据传输之前已经获得,因此要取reg_addr 2.数据传输和地址传输同时到达(大部分情况),因此直接取awaddr
   assign write_addr = (awvalid && awready) ? awaddr : reg_awaddr;
   always @(*) begin
-    if(sram_write_state == WS1) begin
+    if(sram_write_state == MEM_WRITE) begin
       n_pmem_write(write_addr, reg_wdata, reg_wstrb);
     end else begin
       n_pmem_write(write_addr, reg_wdata, 0);
@@ -205,58 +198,62 @@ module axi_sram  #(SRAM_READ_CYCLE = 1)(
   end
 
   reg wait_for_bresp;
+  // always @(posedge aclk) begin
+  //   if(areset) begin
+  //     bvalid <= 0;
+  //     bresp  <= 1; // 1代表exokay
+  //     wait_for_bresp <= 0;
+  //   end else begin
+  //     if(wait_for_bresp) begin
+  //       if(bready) begin
+  //         bvalid <= 0;
+  //         bresp <= 1;
+  //         wait_for_bresp <= 0;
+  //       end
+  //     end else begin
+  //       if(sram_write_state == WS1) begin
+  //         assert(bvalid == 0);
+  //         assert(bresp == 1);
+  //         bvalid <= 1;
+  //         bresp <= 0;
+  //         if(!bready) wait_for_bresp <= 1;
+  //       end else begin
+  //         if(bvalid && bready) begin
+  //           bvalid <= 0;
+  //           bresp  <= 1;
+  //         end
+  //       end
+  //     end
+  //   end
+  // end
+
   always @(posedge aclk) begin
     if(areset) begin
-      bvalid <= 0;
-      bresp  <= 1; // 1代表exokay
+      bvalid         <= 0;
+      bresp          <= 1;
       wait_for_bresp <= 0;
     end else begin
-      if(wait_for_bresp) begin
-        if(bready) begin
-          bvalid <= 0;
-          bresp <= 1;
-          wait_for_bresp <= 0;
-        end
-      end else begin
-        if(sram_write_state == WS1) begin
-          assert(bvalid == 0);
-          assert(bresp == 1);
-          bvalid <= 1;
-          bresp <= 0;
-          if(!bready) wait_for_bresp <= 1;
+      if(sram_write_state == MEM_WRITE) begin
+        if(wait_for_bresp) begin
+          if(bready) begin
+            bvalid          <= 0;
+            bresp           <= 1;
+            wait_for_bresp  <= 0;
+          end
         end else begin
           if(bvalid && bready) begin
-            bvalid <= 0;
-            bresp  <= 1;
-          end
+            bvalid  <= 0;
+            bresp   <= 1;
+          end else begin
+            assert(bvalid == 0);
+            assert(bresp  == 1);
+            bvalid  <= 1;
+            bresp   <= 0;
+            if(!bready) wait_for_bresp <= 1;
+          end 
         end
       end
     end
-  end
-  parameter WS0 = 0, WS1 = 1;
-  reg sram_write_state, sram_write_next_state;
-  always @(posedge aclk) begin
-    if(areset) begin
-      sram_write_state <= WS0;
-    end else begin
-      sram_write_state <= sram_write_next_state;
-    end
-  end
-
-  always@(sram_write_state or wvalid or wready) begin
-    case(sram_write_state)
-    WS0: begin
-      if(wvalid && wready) begin
-        sram_write_next_state = WS1;
-      end else begin
-        sram_write_next_state = WS0;
-      end
-    end
-    WS1: begin
-      sram_write_next_state = WS0;
-    end
-    default: begin end // do nothing
-    endcase
   end
 
 endmodule
