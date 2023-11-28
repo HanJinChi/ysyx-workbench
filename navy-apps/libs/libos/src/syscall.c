b1d7ecdf4dc2a75204993faa78aa7e5a0cb91a57
tracer-ysyx <tracer@ysyx.org>
2023-11-28 20:37:48 +0800

    > sim RTL ysyx_22040000 张三 Linux london 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:37:48 up 24 days, 10:10,  1 user,  load average: 0.58, 1.01, 0.95

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -31,7 +31,10 @@
 #elif defined(__ISA_MIPS32__)
 # define ARGS_ARRAY ("syscall", "v0", "a0", "a1", "a2", "v0")
 #elif defined(__ISA_RISCV32__) || defined(__ISA_RISCV64__)
-# define ARGS_ARRAY ("ecall", "a7", "a0", "a1", "a2", "a0")
+#ifdef __riscv_e
+  # define ARGS_ARRAY ("ecall", "a5", "a0", "a1", "a2", "a0")
+#else
+  # define ARGS_ARRAY ("ecall", "a7", "a0", "a1", "a2", "a0")
 #elif defined(__ISA_AM_NATIVE__)
 # define ARGS_ARRAY ("call *0x100000", "rdi", "rsi", "rdx", "rcx", "rax")
 #elif defined(__ISA_X86_64__)
