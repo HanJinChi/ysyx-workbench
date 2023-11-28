2cdfb39a9792a33444ad2d8f5558c5aa8e154ce8
tracer-ysyx <tracer@ysyx.org>
2023-11-28 15:24:30 +0800

    > sim RTL ysyx_22040000 张三 Linux london 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:24:30 up 24 days,  4:56,  1 user,  load average: 0.99, 1.51, 1.21

--- abstract-machine/am/src/riscv/npc/gpu.c
+++ abstract-machine/am/src/riscv/npc/gpu.c
@@ -4,12 +4,12 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  // int config = inl(VGACTL_ADDR);
-  // int w = config >> 16;
-  // int h = config & 0xffff;
+  int config = inl(VGACTL_ADDR);
+  int w = config >> 16;
+  int h = config & 0xffff;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   putstr("gpu init 0 \n");
-  for( int i = 0; i < 4*4; i++) fb[i] = i;
+  for( int i = 0; i < w*h; i++) fb[i] = i;
   outl(SYNC_ADDR, 1);
   putstr("gpu init 1\n");
 }
