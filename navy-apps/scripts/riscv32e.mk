b92f26b76a599ef6e69e296d5bd60fad52c9cc88
tracer-ysyx <tracer@ysyx.org>
2023-11-29 09:45:45 +0800

    > sim RTL ysyx_22040000 张三 Linux london 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:45:45 up 24 days, 23:18,  1 user,  load average: 0.41, 0.98, 1.01

--- navy-apps/scripts/riscv32e.mk
+++ navy-apps/scripts/riscv32e.mk
@@ -1,3 +1,8 @@
 include $(NAVY_HOME)/scripts/riscv/common.mk
-CFLAGS  += -march=rv32em -mabi=ilp32e  #overwrite
+CFLAGS  += -march=rv32e_zicsr -mabi=ilp32e  #overwrite
 LDFLAGS += -melf32lriscv
+
+SRCS += $(NAVY_HOME)/libs/libgcc/div.S \
+		$(NAVY_HOME)/libs/libgcc/muldi3.S \
+		$(NAVY_HOME)/libs/libgcc/multi3.S \
+		$(NAVY_HOME)/libs/libgcc/ashldi3.S
