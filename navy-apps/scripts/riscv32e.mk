1dc7c5d5134568325b29af67ddd81e2306677eab
tracer-ysyx <tracer@ysyx.org>
2023-11-29 09:33:37 +0800

    > compile NEMU ysyx_22040000 张三 Linux london 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:33:37 up 24 days, 23:06,  1 user,  load average: 1.92, 1.29, 0.93

--- navy-apps/scripts/riscv32e.mk
+++ navy-apps/scripts/riscv32e.mk
@@ -1,9 +1,3 @@
-CROSS_COMPILE = riscv64-linux-gnu-
-LNK_ADDR = $(if $(VME), 0x40000000, 0x83000000)
-CFLAGS  += -fno-pic -march=rv32e_zicsr -mabi=ilp32e
-LDFLAGS += -melf32lriscv --no-relax -Ttext-segment $(LNK_ADDR)
-
-SRCS += $(NAVY_HOME)/libs/libgcc/div.S \
-		$(NAVY_HOME)/libs/libgcc/muldi3.S \
-		$(NAVY_HOME)/libs/libgcc/multi3.S \
-		$(NAVY_HOME)/libs/libgcc/ashldi3.S
+include $(NAVY_HOME)/scripts/riscv/common.mk
+CFLAGS  += -march=rv32em -mabi=ilp32e  #overwrite
+LDFLAGS += -melf32lriscv
--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -11,7 +11,6 @@ int main() {
   assert(bmp);
   NDL_OpenCanvas(&w, &h);
   NDL_DrawRect(bmp, 0, 0, w, h);
-
   free(bmp);
   NDL_Quit();
   printf("Test ends! Spinning...\n");
