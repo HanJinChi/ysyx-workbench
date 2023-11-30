ad33b271ab59cd29caabce70fa9f87012821d8af
tracer-ysyx <tracer@ysyx.org>
2023-11-30 23:24:54 +0800

    > compile NEMU ysyx_22040000 张三 Linux london 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:24:54 up  6:49,  1 user,  load average: 5.80, 4.44, 3.08

--- npc/csrc/src/monitor/sdb/sdb.cc
+++ npc/csrc/src/monitor/sdb/sdb.cc
@@ -1,24 +1,12 @@
-/***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
-
+#include "sdb.h"
+#include "cpu/difftest.h"
+#include <bits/types/FILE.h>
+#include <cstdio>
 #include <isa.h>
 #include <cpu/cpu.h>
+#include <memory/paddr.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-#include <memory/paddr.h>
-#include "sdb.h"
 
 static int is_batch_mode = false;
 
@@ -26,7 +14,7 @@ void init_regex();
 void init_wp_pool();
 void init_bp_pool();
 
-/* We use the `readline' library to provide more flexibility to read from stdin. */
+
 static char* rl_gets() {
   static char *line_read = NULL;
 
@@ -35,7 +23,7 @@ static char* rl_gets() {
     line_read = NULL;
   }
 
-  line_read = readline("(nemu) ");
+  line_read = readline("(npc) ");
 
   if (line_read && *line_read) {
     add_history(line_read);
@@ -47,11 +35,6 @@ static char* rl_gets() {
 static int cmd_c(char *args) {
   cpu_exec(-1);
   return 0;
-}
-
-static int cmd_q(char *args) {
-  nemu_state.state = NEMU_QUIT;
-  return -1;
 }
 
 static int cmd_si(char *args){
@@ -66,14 +49,17 @@ static int cmd_si(char *args){
   return 0;
 }
 
+static int cmd_q(char *args) {
+  npc_state.state = NPC_QUIT;
+  return -1;
+}
+
 static int cmd_info(char *args){
   if(strcmp(args, "r") == 0){
     isa_reg_display();
-  }
-  else if(strcmp(args, "w") == 0){
+  }else if(strcmp(args, "w") == 0){
     info_watchpoint();
-  }
-  else if(strcmp(args, "b") == 0){
+  }else if(strcmp(args, "b") == 0){
     info_breakpoint();
   }
   return 0;
@@ -82,6 +68,7 @@ static int cmd_info(char *args){
 static int cmd_x(char *args){
   int N;
   word_t value;
+  int j = 0;
   // extract first parameters 
   char* args_0 = strtok(args, " ");
   /* treat the remaining string as the arguments,
@@ -91,7 +78,8 @@ static int cmd_x(char *args){
   
   // Convert a string to a number
   bool success = false;
-  N = atoi(args_0); value = expr(args, &success); // TODO : EXPR
+  N = atoi(args_0); 
+  value = expr(args, &success); // TODO : EXPR
   for(int i = 0; i < N/16; i++){
     for(int j = 0; j<16; j++){
       if(j == 0) 
@@ -140,10 +128,20 @@ static int cmd_db(char *args){
   return 0;
 }
 
+static int cmd_detach(char *args){
+  difftest_detach();
+  return 0;
+}
+
+static int cmd_attach(char *args){
+  difftest_attach();
+  return 0;
+}
+
 static int cmd_save(char *args){
   FILE *fp = fopen(args, "wb");
 
-  fwrite(guest_to_host(RESET_VECTOR), CONFIG_MSIZE, 1, fp);
+  fwrite(guest_to_host(RESET_VECTOR), MSIZE, 1, fp);
   fwrite(&cpu, sizeof(CPU_state), 1, fp);
   fclose(fp);
 
@@ -152,26 +150,22 @@ static int cmd_save(char *args){
 
 static int cmd_load(char *args){
   FILE *fp = fopen(args, "rb");
-  int ret = 0;
 
-  ret = fread(guest_to_host(RESET_VECTOR), CONFIG_MSIZE, 1, fp);
-  ret = fread(&cpu, sizeof(CPU_state), 1, fp);
+  fread(guest_to_host(RESET_VECTOR), MSIZE, 1, fp);
+  fread(&cpu, sizeof(CPU_state), 1, fp);
 
   fclose(fp);
-  return ret;
+  return 0;
 }
-
-static int cmd_help(char *args);
 
 static struct {
   const char *name;
   const char *description;
   int (*handler) (char *);
 } cmd_table [] = {
-  { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
-  { "q", "Exit NEMU", cmd_q },
-  { "si", "Pause Nemu after running [N] instructions", cmd_si },
+  { "q", "Exit NPC", cmd_q },
+  { "si", "Pause NPC after running [N] instructions", cmd_si },
   { "info", "Print relevant information", cmd_info },
   { "x", "Scan Memory", cmd_x },
   { "p", "Calculate the value of [expr]", cmd_p },
@@ -179,37 +173,15 @@ static struct {
   { "b", "set breakpoint", cmd_b },
   { "dw", "delete all watchpoint", cmd_dw },
   { "db", "delete all breakpoint", cmd_db },
+  {"detach", "exit difftest", cmd_detach},
+  {"attach", "start difftest", cmd_attach},
   {"save", "save cpu and memory state to path", cmd_save},
   {"load", "load cpu and memory state to path", cmd_load}
-
   /* TODO: Add more commands */
 
 };
 
 #define NR_CMD ARRLEN(cmd_table)
-
-static int cmd_help(char *args) {
-  /* extract the first argument */
-  char *arg = strtok(NULL, " ");
-  int i;
-
-  if (arg == NULL) {
-    /* no argument given */
-    for (i = 0; i < NR_CMD; i ++) {
-      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-    }
-  }
-  else {
-    for (i = 0; i < NR_CMD; i ++) {
-      if (strcmp(arg, cmd_table[i].name) == 0) {
-        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-        return 0;
-      }
-    }
-    printf("Unknown command '%s'\n", arg);
-  }
-  return 0;
-}
 
 void sdb_set_batch_mode() {
   is_batch_mode = true;
@@ -240,7 +212,6 @@ void sdb_mainloop() {
     extern void sdl_clear_event_queue();
     sdl_clear_event_queue();
 #endif
-
     int i;
     for (i = 0; i < NR_CMD; i ++) {
       if (strcmp(cmd, cmd_table[i].name) == 0) {
@@ -253,13 +224,10 @@ void sdb_mainloop() {
   }
 }
 
-void init_sdb() {
-  /* Compile the regular expressions. */
+void init_sdb(){
   init_regex();
 
-  /* Initialize the watchpoint pool. */
   init_wp_pool();
 
-  /* Initialize the breakpoint pool. */
   init_bp_pool();
 }
