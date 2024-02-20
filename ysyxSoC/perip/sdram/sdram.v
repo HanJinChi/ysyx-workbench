bf516cf76fc909cb67ba12e18742ff23a7caa2ec
hanjinchi <hanjinchi00@outlook.com>
2024-02-20 23:42:48 +0800

    add sdram part

--- ysyxSoC/perip/sdram/sdram.v
+++ ysyxSoC/perip/sdram/sdram.v
@@ -5,12 +5,22 @@ module sdram(
   input        ras,
   input        cas,
   input        we,
-  input [12:0] a,
-  input [ 1:0] ba,
-  input [ 1:0] dqm,
-  inout [15:0] dq
+  input [12:0] a,   // 地址
+  input [ 1:0] ba,  // 存储体地址
+  input [ 1:0] dqm, // 数据掩码
+  inout [15:0] dq   
 );
 
+  localparam SDRAM_BANK_W          = 2;
+  localparam SDRAM_DQM_W           = 2;
+  localparam SDRAM_BANKS           = 2 ** SDRAM_BANK_W;
+
+  reg out;
   assign dq = 16'bz;
+  wire [15:0] din;
+  assign din = dq;
+  assign dq = out ? dout : 16'bz;
+
+
 
 endmodule
